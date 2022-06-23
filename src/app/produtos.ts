52f206e0b40987de98c89c186f75c773b1e23cb9
Matheus Santos <matheus.santos.redes@gmail.com>
2022-06-23 10:53:00 -0300

    build

---
+++ src/app/produtos.ts
@@ -0,0 +1,30 @@
+export interface IProduto {
+  id: number;
+  descricao: string;
+  preco: number;
+  descricaoPreco: string;
+  quantidadeEstoque: number;
+  imagem: string;
+}
+
+export interface IProdutoCarrinho extends IProduto {
+  quantidade: number;
+}
+
+export const produtos: IProduto[] = [
+  { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista", imagem: "/assets/mouse-3.jpg",quantidadeEstoque: 23 },
+  { id: 2, descricao: "Monitor muito bom", preco: 1200.50, descricaoPreco: "À vista ", imagem: "/assets/monitor-1.jpg" ,quantidadeEstoque: 60 },
+  { id: 3, descricao: "Teclado excelente", preco: 749.99, descricaoPreco: "À vista", imagem: "/assets/teclado-1.jpg",quantidadeEstoque: 31  },
+  { id: 4, descricao: "Fone para quem joga FPS", preco: 599.99, descricaoPreco: "À vista", imagem: "/assets/fone-de-ouvido-2.jpg",quantidadeEstoque: 21  },
+  { id: 5, descricao: "Fone de ouvido", preco: 299.99, descricaoPreco: "À vista", imagem: "/assets/fone-de-ouvido-1.jpg", quantidadeEstoque: 57},
+  { id: 6, descricao: "Fone de ouvido bom", preco: 399.99, descricaoPreco: "À vista", imagem: "/assets/fone-de-ouvido-3.jpg" ,quantidadeEstoque: 86  },
+  { id: 7, descricao: "HD 1TB", preco: 499.99, descricaoPreco: "À vista", imagem: "/assets/hd.jpg" ,quantidadeEstoque: 16 },
+  { id: 8, descricao: "Combo de placa de vÃ­deos", preco: 18449.99, descricaoPreco: "À vista", imagem: "/assets/placa-video.jpg",quantidadeEstoque: 9  },
+  { id: 9, descricao: "Processador Ryzen", preco: 1000, descricaoPreco: "À vista", imagem: "/assets/processador.jpg",quantidadeEstoque: 20  },
+  { id: 10, descricao: "Notebook bom", preco: 2500, descricaoPreco: "À vista", imagem: "/assets/laptop-1.jpg" ,quantidadeEstoque: 11 },
+  { id: 11, descricao: "Notebook excelente", preco: 4500, descricaoPreco: "À vista", imagem: "/assets/laptop-2.jpg" ,quantidadeEstoque: 13 },
+  { id: 12, descricao: "Mouse barato", preco: 20, descricaoPreco: "À vista", imagem: "/assets/mouse-1.png",quantidadeEstoque: 14  },
+  { id: 13, descricao: "Mouse Ã³timo", preco: 200, descricaoPreco: "À vista", imagem: "/assets/mouse-2.jpg",quantidadeEstoque: 32  },
+  { id: 14, descricao: "Mouse pequeno", preco: 50, descricaoPreco: "À vista", imagem: "/assets/mouse-4.jpg" ,quantidadeEstoque: 8 },
+  { id: 15, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista ", imagem: "/assets/teclado-2.jpg",quantidadeEstoque: 5  },
+]
